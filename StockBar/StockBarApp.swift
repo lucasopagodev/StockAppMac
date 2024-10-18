3ecb0cf734ecc4b56aa3914113de7c41d9824dce
Lucas Rodrigues <rodrigues14set@hotmail.com>
2024-10-18 17:07:11 -0300

    Initial Commit

---
+++ StockBar/StockBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockBarApp.swift
+//  StockBar
+//
+//  Created by Lucas Rodrigues on 18/10/24.
+//
+
+import SwiftUI
+
+@main
+struct StockBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
